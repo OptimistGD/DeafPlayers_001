00e0bec442bfa3ec59719b98ccd62acb99e9496c
OptimistGD <optimist.professionel@gmail.com>
2026-08-11 17:32:23 +0200

    Collection 2/?

--- DeafPlayers_001/Assets/Script/Gameplay/CardCollection/Logic/CardCollection.cs
+++ DeafPlayers_001/Assets/Script/Gameplay/CardCollection/Logic/CardCollection.cs
@@ -1,25 +1,57 @@
 ﻿using System.Collections.Generic;
 using DeafPlayers.Gameplay.Script.Gameplay.Data;
+using UnityEngine;
 
 namespace DeafPlayers.Gameplay.Script.Gameplay.Logic
 {
-    public class CardCollection
+    public class CardCollection 
     {
-        private readonly Card[] collection;
+        public void CreateAllCard()
+        {
+            CardDataBase dataBase = Resources.Load<CardDataBase>("DataBase");
+            
+            for (int j = 0; j < dataBase.CollectionSize; j++)
+            {
+                CardData data = dataBase.DataBase[j];
+                Card card = new Card(data, j);
 
+                cardCollectionFull.TryAdd(data, card);
+            }
+        }
+        
+        private readonly Card[] currentCollection;
+
+        private readonly Dictionary<CardData, Card> cardCollectionFull;
+        
+        //TODO a modif car deja accès a CardDataBase
         private CardCollection(CardDataBase cardDataBase)
         {
-            collection = new Card[cardDataBase.CollectionSize];
+            cardCollectionFull = new();
+            currentCollection = new Card[cardDataBase.CollectionSize];
+            
+            CreateAllCard();
         }
 
-        public bool TryAddCard(Card card)
+
+        public bool TryAddCard(CardData cardData)
         {
-            if (collection[card.Index] != null)
+            if (!cardCollectionFull.TryGetValue(cardData, out Card card))
             {
                 return false;
             }
             
-            collection[card.Index] = card;
+            return TryAddCard(card);
+        }
+        
+        
+        public bool TryAddCard(Card card)
+        {
+            if (currentCollection[card.Index] != null)
+            {
+                return false;
+            }
+            
+            currentCollection[card.Index] = card;
             
             return true;
         }
---
+++ DeafPlayers_001/Assets/Script/Gameplay/CardCollection/Visual/MonoCard.cs
@@ -0,0 +1,28 @@
+﻿using DeafPlayers.Gameplay.Script.Gameplay.Data;
+using DeafPlayers.Gameplay.Script.Gameplay.Logic;
+using UnityEngine;
+
+namespace DeafPlayers.Gameplay.Script.Gameplay.Visual
+{
+    public class MonoCard : MonoBehaviour
+    {
+        [SerializeField] private CardData cardData;
+
+        public CardData RequestData()
+        {
+            return cardData;
+        }
+
+
+        /* PAS ICI
+        public void blabla()
+        {
+            var T = new MonoCard();
+            CardData cardData = T.RequestData();
+            
+            CardCollection.TryAddCard(cardData);
+        }
+        */
+
+    }
+}
